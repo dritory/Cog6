66cb295d9b99ef1435a4a3711bdcd225c6f6a837
theAzack9 <robin@cavachi.be>
2018-01-24 21:32:54 +0100

    Added luajit/sol2 and moved sfml to libraries folder

--- Main.cpp
+++ Main.cpp
@@ -1,17 +1,34 @@
 #include <SFML/Graphics.hpp>
+#include <sol.hpp>
+#include <cassert>
 
 int main()
 {
 	sf::RenderWindow window(sf::VideoMode(800, 600),
 		"Hello SFML", sf::Style::Default);
 
+	window.setFramerateLimit(60);
+
 	sf::Font font;
-	font.loadFromFile("C:/Windows/Fonts/Arial.ttf");
+	font.loadFromFile("C:/Windows/Fonts/SegoeUI.ttf");
 
 	sf::Text text;
 	text.setFont(font);
 	text.setPosition(200, 200);
 	text.setString("Hello World, hello SFML");
+	
+	sol::state lua;
+	lua.set_function("getString", [] { return "Hello from luajit"; });
+	auto result = lua.script("return getString()");
+
+	assert(result.valid());
+
+	const std::string str = result;
+
+	sf::Text text2;
+	text2.setFont(font);
+	text2.setPosition(200, 250);
+	text2.setString(str);
 
 	while (window.isOpen())
 	{
@@ -23,6 +40,7 @@ int main()
 		}
 		window.clear();
 		window.draw(text);
+		window.draw(text2);
 		window.display();
 	}
 
